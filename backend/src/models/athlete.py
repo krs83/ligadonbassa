92d828749970fb225ba5afaecaa0ec8d4af381f5
krs83 <krs83@yandex.ru>
2026-01-18 14:14:53 +0300

    fix: Добавил нормализацию для полей activity  и points

--- backend/src/models/athlete.py
+++ backend/src/models/athlete.py
@@ -1,4 +1,5 @@
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
+from pydantic import field_validator
 
 from sqlmodel import Field, SQLModel, String, Relationship
 
@@ -14,6 +15,18 @@ class AthleteBase(SQLModel):
     academy: str = Field(String(50), index=True, nullable=True)
     activity: float = Field(default=1.0, nullable=False, ge=0.5, le=1.5)
     points: int = Field(index=True, default=0, ge=0)
+
+    @field_validator("activity", mode="before")
+    def normalize_activity(cls, v: Any):
+        if v is None:
+            return None
+        return max(0.5, min(v, 1.5))
+
+    @field_validator("points", mode="before")
+    def normalize_points(cls, v: Any):
+        if v is None:
+            return None
+        return max(0, v)
 
 
 class Athlete(AthleteBase, table=True):
@@ -40,12 +53,10 @@ class AthleteCreate(AthleteBase):
     tournament_ids: list[int] = []
 
 
-class AthleteUpdate(SQLModel):
+class AthleteUpdate(AthleteBase):
     fullname: str | None = None
     category: str | None = None
     academy: str | None = None
     activity: float | None = None
     points: int | None = None
     tournament_ids: list[int] | None = None
-
-
